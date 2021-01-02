e77b910443a2c7fd915783bb36fc3196f37c5153
samipourquoi <sami.guiderdoni@gmail.com>
2021-01-02 20:54:30 +0100

    Make blind opening animation in pure CSS

--- public/js/home.js
+++
@@ -1,6 +0,0 @@
-const wrap = document.querySelector('.wrap'), nav = document.querySelector('header'), tl = gsap.timeline();
-
-tl.set(wrap, {height: "0%"})
-    .set(nav, {opacity: 0})
-    .to(wrap, 1, {height: "+=100%", autoRound: false, ease: Power2.easeInOut}, "+=1")
-    .to(nav, 0.5, {opacity: 1, ease: Power2.easeInOut});
